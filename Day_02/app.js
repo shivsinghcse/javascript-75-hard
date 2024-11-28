b37e4a36f04ef2c544f7665c4b1df066b29c043d
Shiv Singh <shivsinghcse@gmail.com>
2024-11-29 05:26:28 +0530

    day 02 completed

---
+++ Day_02/app.js
@@ -0,0 +1,11 @@
+// alert()
+document.body.innerHTML = 'vhvfvfnvjhfnjhn';
+// document.writeln("Shiv");
+// document.writeln("Hello World!");
+// document.writeln("Have a nice day!");
+console.log("Hello");
+console.warn("Don't click me!");
+console.error("Error: Payment Failed!");
+document.writeln('<h1>Hello</h1>');
+document.writeln('<h1 style="color: red; font-size: 100px;">Hello</h1>');
+document.writeln('<h1 style="color: blue; font-size: 20px;">I am testing</h1>');
