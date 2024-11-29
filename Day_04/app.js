b2f6ef2cd0ce37bbf779aa837e2f7ccd3d950345
Shiv Singh <shivsinghcse@gmail.com>
2024-11-29 20:40:03 +0530

    day-04 completed

---
+++ Day_04/app.js
@@ -0,0 +1,26 @@
+// alert()
+
+// var x = 2;
+// var y = 3;
+// var z = x + y;
+// alert(z)
+
+var a = 2;
+var b = 4;
+var c = 3;
+var d = 90;
+var e = 12;
+
+var f = a + b + c + d + e;
+console.log(f);
+
+var institute = 'codingOtt';
+var my_institute;
+var myInstitute;
+// alert(institute);
+
+function greet(name) {
+    console.log(`${name}, welcome to GFG!`);
+}
+
+// greet('Geek');
