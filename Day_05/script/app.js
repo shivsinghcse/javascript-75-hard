30b7f55e0be71f8288b9088bdd4add2c70234d34
Shiv Singh <shivsinghcse@gmail.com>
2024-11-30 00:53:29 +0530

    day-05 completed

---
+++ Day_05/script/app.js
@@ -0,0 +1,27 @@
+// alert();
+
+// const body = document.getElementById('body');
+
+function changeBackground(color) {
+    document.body.style.backgroundColor = color;
+}
+
+function person1() {
+    alert('Person 1 name is saurav');
+}
+function person2() {
+    alert('Person 2 name is priya');
+}
+function person3() {
+    alert('Person 3 name is Rahul');
+}
+function person4() {
+    alert('Person 4 name is Shiv');
+}
+
+function right(){
+    alert('This is right answer')
+}
+function wrong(){
+    alert('This is wrong answer')
+}
