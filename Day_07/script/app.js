53efc196b09a761d837a9821af1f9c1979545421
Shiv Singh <shivsinghcse@gmail.com>
2024-11-30 03:57:05 +0530

    day-07 completed

---
+++ Day_07/script/app.js
@@ -0,0 +1,18 @@
+// function red() {
+//     var h1 = document.getElementById('heading');
+//     h1.style.color = 'red';
+// }
+
+function colorChange(color, bgcolor){
+    var h1 = document.getElementById('heading');
+    h1.style.color = color;
+    h1.style.backgroundColor = bgcolor;
+}
+// function blue(){
+//     var h1 = document.getElementById('heading');
+//     h1.style.color = 'blue';
+// }
+// function pink(){
+//     var h1 = document.getElementById('heading');
+//     h1.style.color = 'pink';
+// }
